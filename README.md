59a5c6b705b42b02ba293e8a34fb6209212cb1ab
douglascorreacavasso <douglas.cavasso@opet.edu.br>
2022-11-21 23:58:35 -0300

    Commit atualizações

---
+++ README.md
@@ -0,0 +1,21 @@
+# REDIRECIONA DISPOSITIVO
+![preview](./.github/preview.png)
+
+> EXEMPLO PARA CLIENTE
+
+Projeto construido para mostra um botão ou link onde após o usuario clicar, 
+ele é redirecionado para uma pagina de acordo com o seu dispositivo.
+
+[ Clique aqui para acessar](https://douglascorreacavasso.github.io/RedirecionaDispositivo/)
+
+
+## Tecnologias
+
+- HTML
+- CSS
+- Git e Github
+- JavaScript
+
+## Contato
+
+douglas.cavasso@gmail.com
--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
+ua = navigator.userAgent,
+			browser = /Edge\/\d+/.test(ua) ? 'ed' : /MSIE 9/.test(ua) ? 'ie9' : /MSIE 10/.test(ua) ? 'ie10' : /MSIE 11/.test(ua) ? 'ie11' : /MSIE\s\d/.test(ua) ? 'ie?' : /rv\:11/.test(ua) ? 'ie11' : /Firefox\W\d/.test(ua) ? 'ff' : /Chrom(e|ium)\W\d|CriOS\W\d/.test(ua) ? 'gc' : /\bSafari\W\d/.test(ua) ? 'sa' : /\bOpera\W\d/.test(ua) ? 'op' : /\bOPR\W\d/i.test(ua) ? 'op' : typeof MSPointerEvent !== 'undefined' ? 'ie?' : '',
+			os = /Windows NT 10/.test(ua) ? "win10" : /Windows NT 6\.0/.test(ua) ? "winvista" : /Windows NT 6\.1/.test(ua) ? "win7" : /Windows NT 6\.\d/.test(ua) ? "win8" : /Windows NT 5\.1/.test(ua) ? "winxp" : /Windows NT [1-5]\./.test(ua) ? "winnt" : /Mac/.test(ua) ? "mac" : /Linux/.test(ua) ? "linux" : /X11/.test(ua) ? "nix" : "",
+			touch = 'ontouchstart' in document.documentElement,
+			mobile = /IEMobile|Windows Phone|Lumia/i.test(ua) ? 'w' : /iPhone|iP[oa]d/.test(ua) ? 'i' : /Android/.test(ua) ? 'a' : /BlackBerry|PlayBook|BB10/.test(ua) ? 'b' : /Mobile Safari/.test(ua) ? 's' : /webOS|Mobile|Tablet|Opera Mini|\bCrMo\/|Opera Mobi/i.test(ua) ? 1 : 0,
+			tablet = /Tablet|iPad/i.test(ua);
+
+result.innerHTML = '' +
+    '<br>ua = ' + ua +
+    '<br>browser = ' + browser +
+    '<br>os = ' + os +
+    '<br>mobile = ' + mobile +
+    '<br>tablet = ' + tablet +
+    '<br>touch = ' + touch +
+    '';
 
 function valida(v){
 var 
@@ -18,27 +33,34 @@ result.innerHTML = '' +
     '';
     var resultado = result
     var voltar = v;
-    if (mobile == 'a'){
-        GoGo(1);
-    }else if (mobile == "i") {
-        GoGo(2);
+    if (mobile == 'a' && voltar == 0){
+        GoGo(1,resultado);
+    }else if (mobile == 'i' && voltar == 0) {
+        GoGo(2,resultado);
     }else if (os.includes("win") && voltar == 0 ) {
-        GoGo(3);
+        GoGo(3,resultado);
     }else if (voltar == 1) {
-        GoGo(4);
+        GoGo(4,resultado);
     }
 }
 
- function GoGo(a) {
+ function GoGo(a,resulta) {
     var pag = a;
+    var resultado = resulta
     if (pag == 1) {
         window.location.href = 'android.html';
+        result.innerHTML = resulta;
     }else if (pag == 2) {
         window.location.href = 'ios.html';
+        result.innerHTML = resulta;
     }else if (pag == 3) {
+        result.innerHTML = resulta;
         window.location.href = 'windows.html';
+       
     }else if (pag == 4) {
+        result.innerHTML = resulta;
         window.location.href = 'index.html';
+        result.innerHTML = resulta;
     }else {
 
     }
